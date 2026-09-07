1d682d2ffe11086d9d2d951b466f87dbe0f8bbf9
jobbbgug-cmd <jobbbgug@gmail.com>
2026-09-08 00:58:09 +0700

    prerender-manifest

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ const nextConfig = {
   },
   distDir: '.next',
   output: 'standalone',
-  onDemandEntries: {
-    maxInactiveAge: 60 * 1000,
-    pagesBufferLength: 5,
-  },
-  experimental: {
-    isrMemoryCacheSize: 0,
-  },
 };
 
 module.exports = nextConfig;
